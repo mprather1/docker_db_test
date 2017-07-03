b70344a2e820cabd5dedf223e77f83e2040b954b
Michael Prather <mprather1@gmail.com>
2017-07-03 22:32:59 +0000

    update

--- src/queries/init.js
+++ src/queries/init.js
@@ -2,20 +2,25 @@ import promise from 'bluebird'
 import pg from 'pg-promise'
 
 export default function initDB (options) {
-  const {environment, config} = options
-
   const pgOptions = {
     promiseLib: promise
   }
 
   const pgp = pg(pgOptions)
-  const connectionString = config.postgresURI[environment]
-  const init = pgp(connectionString)
-  const databaseName = connectionString.split('/')
+  // const connectionString = config.postgresURI[environment]
+  const init = pgp({
+    host: process.env['DB_HOST'],
+    port: process.env['DB_PORT'],
+    user: process.env['DB_USER'],
+    password: process.env['DB_PASSWORD'],
+    database: process.env['DATABASE']
 
-  if (environment === 'development') {
-    console.log('Connected to database: ' + databaseName[databaseName.length - 1])
-  }
+  })
+  // const databaseName = connectionString.split('/')
+
+  // if (environment === 'development') {
+    // console.log('Connected to database: ' + databaseName[databaseName.length - 1])
+  // }
 
   return init
 }
